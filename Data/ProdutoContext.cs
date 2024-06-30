2922b2eec717b5220550572cf5c1b747b5b6cab5
ariel-carvalho <ariell_carvalho@hotmail.com>
2024-06-30 19:07:01 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ProdutoContext.cs
@@ -0,0 +1,11 @@
+﻿using ComexAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ComexAPI.Data;
+
+public class ProdutoContext(DbContextOptions<ProdutoContext> options) : DbContext(options)
+{
+    public DbSet<Produto> Produtos { get; set; }
+}
